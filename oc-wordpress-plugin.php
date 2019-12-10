c827d1f84795fb14b447faeec25ca9cd19fa395e
Brandon Dove <brandon@pixeljar.net>
2019-12-10 09:19:44 -0800

    Adds main plugin file

---
+++ oc-wordpress-plugin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: OC WordPress Plugin
+ * Description: This plugin shows many examples of core WordPress functionality that could be built into a plugin.
+ * Plugin URI: https://www.pixeljar.com
+ * Author: Pixel Jar
+ * Author URI: http://www.pixeljar.com
+ * Version: 1.0
+ * License: GPL2
+ * Text Domain: ocwp
+ * Domain Path: /lang
+ *
+ * Copyright (C) Dec 10, 2019  Pixel Jar  info@pixeljar.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package WordPress
+ */
+
+// Only proceed if this file is being loaded through WordPress.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'This file must not be called directly.' );
+}
